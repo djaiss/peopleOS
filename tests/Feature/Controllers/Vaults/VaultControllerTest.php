6568b7cd8f76597bb122cd6c655c4f4af51d748b
Mazarin <djaiss@users.noreply.github.com>
2024-07-04 08:10:07 -0400

    wip

---
+++ tests/Feature/Controllers/Vaults/VaultControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Controllers\Vaults;
+
+use App\Models\User;
+use App\Models\Vault;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class VaultControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    public function it_shows_an_empty_dashboard_upon_account_creation(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->get('/')
+            ->assertSee('Thanks for');
+    }
+
+    #[Test]
+    public function a_user_can_create_a_vault(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->post('/')
+            ->assertSee('henri');
+
+        $this->actingAs($user)
+            ->post('/new', [
+                'name' => 'Super vault',
+                'description' => 'this is the description',
+            ])
+            ->assertRedirectToRoute('vaults.show', ['vault' => Vault::first()]);
+    }
+}
