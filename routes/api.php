4ee3cac8d73fe88d60f561b2670686e3711e0bad
Mazarin <djaiss@users.noreply.github.com>
2024-07-08 15:58:12 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\Settings\MeController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
