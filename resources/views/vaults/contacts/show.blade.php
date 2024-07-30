d15a78ffe29cae824ce7ec8a024057b1acefaa5f
Regis Freyd <djaiss@users.noreply.github.com>
2024-07-30 08:28:59 -0400

    feat: add child

--- resources/views/vaults/contacts/show.blade.php
+++ resources/views/vaults/contacts/show.blade.php
@@ -81,6 +81,7 @@
 
               <!-- kids -->
               <div class="relative mb-8 rounded-lg border border-gray-200 bg-gray-50 p-3 dark:border-gray-700 dark:bg-gray-900">
+                <!-- plus button -->
                 <div class="absolute -top-4 w-full">
                   <div class="flex justify-between">
                     <div class="flex items-center rounded-full border bg-white py-1 pl-2 pr-3 text-sm">
@@ -99,16 +100,78 @@
                   </div>
                 </div>
 
-                <ul class="mt-2">
-                  <li>
-                    Regis
-                    <span class="text-xs text-gray-600">(8)</span>
-                  </li>
-                  <li>
-                    Lorraine
-                    <span class="text-xs text-gray-600">(8)</span>
-                  </li>
-                </ul>
+                <!-- add kid form -->
+                <div>
+                  <!-- choose gender -->
+                  <div class="mb-2 mt-4 grid grid-cols-3 space-x-2">
+                    <div class="flex flex-col items-center rounded-lg border border-gray-200 py-3">
+                      <input type="radio" class="mb-1" />
+                      <span class="text-2xl">
+                        <svg class="h-6 w-6" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" fill="#000000">
+                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                          <g id="SVGRepo_iconCarrier">
+                            <path fill="#e0f9cb" d="M6 20c0 2.209-1.119 4-2.5 4S1 22.209 1 20s1.119-4 2.5-4S6 17.791 6 20zm29 0c0 2.209-1.119 4-2.5 4S30 22.209 30 20s1.119-4 2.5-4s2.5 1.791 2.5 4z"></path>
+                            <path fill="#e0f9cb" d="M4 20.562c0-8.526 6.268-15.438 14-15.438s14 6.912 14 15.438S25.732 35 18 35S4 29.088 4 20.562z"></path>
+                            <path fill="#335e32" d="M12 22a1 1 0 0 1-1-1v-2a1 1 0 0 1 2 0v2a1 1 0 0 1-1 1zm12 0a1 1 0 0 1-1-1v-2a1 1 0 1 1 2 0v2a1 1 0 0 1-1 1z"></path>
+                            <path fill="#00b512" d="M18 30c-4.188 0-6.357-1.06-6.447-1.105a1 1 0 0 1 .89-1.791c.051.024 1.925.896 5.557.896c3.665 0 5.54-.888 5.559-.897a1.003 1.003 0 0 1 1.336.457a.997.997 0 0 1-.447 1.335C24.356 28.94 22.188 30 18 30zm1-5h-2a1 1 0 1 1 0-2h2a1 1 0 1 1 0 2z"></path>
+                            <path fill="#44b042" d="M18 .354C8.77.354 3 6.816 3 12.2c0 5.385 1.154 7.539 2.308 5.385l2.308-4.308s3.791-.124 6.099-2.278c0 0-1.071 4 6.594.124c0 0-.166 3.876 5.191-.124c0 0 4.039 1.201 5.191 6.586c.32 1.494 2.309 0 2.309-5.385C33 6.816 28.385.354 18 .354z"></path>
+                          </g>
+                        </svg>
+                      </span>
+                      <span class="text-xs">{{ __('Boy') }}</span>
+                    </div>
+                    <div class="flex flex-col items-center rounded-lg border border-gray-200 py-3">
+                      <input type="radio" class="mb-1" />
+                      <span class="text-2xl">
+                        <svg class="h-6 w-6" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" fill="#000000">
+                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                          <g id="SVGRepo_iconCarrier">
+                            <path d="M57.9 40S61 34.4 61 26c0-11-8.1-24-29-24S3 15 3 26c0 8.4 3.1 14 3.1 14C2.5 44.1.3 52.4 3.4 57.8c1.3 2.2 14.1 10 15.3-3.9h26.4c1.3 13.8 14.1 6.1 15.3 3.9c3.2-5.4 1-13.7-2.5-17.8" fill="#f367a3"></path>
+                            <path d="M32 6C16.7 6 5.5 14 5.5 24C5.5 24 9.6 9 32 9s25.5 15 25.5 15C57.5 14 47.3 6 32 6z" fill="#c28fef"></path>
+                            <path d="M57.3 42c6.2 0 6.2-9 0-9v-3s-35-.9-43-13c2 12.2-7.7 16-7.7 16c-6.2 0-6.2 9 0 9c0 7 7.3 18 25.3 18s25.4-11 25.4-18" fill="#f9e9cf"></path>
+                            <g fill="#ffffff">
+                              <circle cx="44.5" cy="36.5" r="6.5"></circle>
+                              <circle cx="19.5" cy="36.5" r="6.5"></circle>
+                            </g>
+                            <circle cx="44.5" cy="36.5" r="4.5" fill="#cd4460"></circle>
+                            <circle cx="44.5" cy="36.5" r="1.5" fill="#231f20"></circle>
+                            <circle cx="19.5" cy="36.5" r="4.5" fill="#cd4460"></circle>
+                            <circle cx="19.5" cy="36.5" r="1.5" fill="#231f20"></circle>
+                            <path d="M40.1 48.1c-5.2 3.6-11 3.6-16.2 0c-.6-.4-1.2.3-.8 1c1.6 2.6 4.8 4.9 8.9 4.9s7.3-2.3 8.9-4.9c.4-.7-.2-1.4-.8-1" fill="#d64426"></path>
+                          </g>
+                        </svg>
+                      </span>
+
+                      <span class="text-xs">{{ __('Girl') }}</span>
+                    </div>
+                    <div class="flex flex-col items-center rounded-lg border border-gray-200 py-3">
+                      <input type="radio" class="mb-1" />
+                      <span class="text-2xl">😶</span>
+                      <span class="text-xs">{{ __('Unknown') }}</span>
+                    </div>
+                  </div>
+
+                  <!-- name -->
+                  <div class="relative mb-4">
+                    <x-input-label for="job_title" :optional="true" :value="__('Name')" />
+
+                    <x-text-input :value="old('job_title', $contact['job_title'])" @keyup.escape="editMode = false" class="mt-1 block w-full" id="job_title" name="job_title" type="text" />
+
+                    <x-input-error class="mt-2" :messages="$errors->get('job_title')" />
+                  </div>
+
+                  <div class="flex justify-between">
+                    <x-button.secondary x-on:click="editMode = false" class="mr-2">
+                      {{ __('Cancel') }}
+                    </x-button.secondary>
+
+                    <x-button.primary type="submit" dusk="update-job-information">
+                      {{ __('Save') }}
+                    </x-button.primary>
+                  </div>
+                </div>
               </div>
 
               <!-- extended family -->
