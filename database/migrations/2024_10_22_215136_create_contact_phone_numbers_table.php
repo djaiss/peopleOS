1f2b58b2686dfa9c15842c069e940b80d211350a
Regis Freyd <djaiss@users.noreply.github.com>
2024-10-23 07:19:23 -0400

    Create 2024_10_22_215136_create_contact_phone_numbers_table.php

---
+++ database/migrations/2024_10_22_215136_create_contact_phone_numbers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_phone_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('contact_id');
+            $table->text('label');
+            $table->text('phone_number');
+            $table->timestamps();
+            $table->foreign('contact_id')->references('id')->on('contacts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_phone_numbers');
+    }
+};
