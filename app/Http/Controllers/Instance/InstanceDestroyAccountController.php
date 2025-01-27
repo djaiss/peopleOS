a42790cd75d78f2696087e39e8a2ad632dbffdc1
Regis Freyd <djaiss@users.noreply.github.com>
2025-01-27 07:55:27 -0500

    feat: delete account as instance admin

---
+++ app/Http/Controllers/Instance/InstanceDestroyAccountController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Instance;
+
+use App\Http\Controllers\Controller;
+use App\Models\Account;
+use App\Services\DestroyAccountAsInstanceAdministrator;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class InstanceDestroyAccountController extends Controller
+{
+    public function destroy(Request $request, Account $account): RedirectResponse
+    {
+        (new DestroyAccountAsInstanceAdministrator(
+            user: Auth::user(),
+            account: $account,
+        ))->execute();
+
+        return redirect()->route('instance.index')
+            ->success(trans('The account has been deleted'));
+    }
+}
