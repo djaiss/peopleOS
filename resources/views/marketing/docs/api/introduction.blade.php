468e420c2aea8f1fdb1d18963d75f6eeca7b7ab2
Regis Freyd <djaiss@users.noreply.github.com>
2025-03-04 07:08:54 -0500

    wip

--- resources/views/marketing/docs/api/introduction.blade.php
+++ resources/views/marketing/docs/api/introduction.blade.php
@@ -4,14 +4,16 @@
   <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
     <div>
       <p class="mb-2">The PeopleOS API is organized around REST. Our API has predictable resource-oriented URLs.</p>
-      <p class="mb-2">You can not use the PeopleOS API in test mode.</p>
+      <p class="mb-2">You can not use the PeopleOS API in test mode. This means all requests will be processed towards your production account. Please be cautious.</p>
       <p class="mb-10">The PeopleOS API doesn’t support bulk updates. You can work on only one object per request.</p>
-
-      <x-marketing.edit-github />
     </div>
     <div>
       <h2 class="text-lg font-bold mb-2">Base URL</h2>
       <x-marketing.code>{{ config('app.url') }}/api</x-marketing.code>
     </div>
+  </div>
+
+  <div>
+    <x-marketing.edit-github />
   </div>
 </x-marketing-docs-layout>
