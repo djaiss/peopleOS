cd03b43093dc6e032d80749b7cd69dec1e489e28
Regis Freyd <djaiss@users.noreply.github.com>
2025-03-10 20:50:28 -0400

    Update MarketingController.php

--- app/Http/Controllers/Marketing/MarketingController.php
+++ app/Http/Controllers/Marketing/MarketingController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Account;
 use App\Services\FetchMergedPRs;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Cache;
 
 class MarketingController extends Controller
 {
@@ -15,7 +16,11 @@ class MarketingController extends Controller
     {
         $accountNumbers = Account::where('created_at', '>=', now()->subWeek())->count();
 
-        $pullRequests = cache()->remember('github_pull_requests', now()->addHours(24), fn (): mixed => (new FetchMergedPRs())->execute());
+        $pullRequests = Cache::remember(
+            key: 'github_pull_requests',
+            ttl: now()->addHours(24),
+            callback: fn (): mixed => (new FetchMergedPRs())->execute()
+        );
 
         return view('marketing.index', [
             'accountNumbers' => $accountNumbers,
