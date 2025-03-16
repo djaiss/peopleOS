cb52820e841b3e9b8225ed3bbb5d11a6a88e90b6
Regis Freyd <djaiss@users.noreply.github.com>
2025-03-16 15:33:55 -0400

    chore: update marketing assets (#138)

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,8 +4,8 @@
     <div class="flex items-center gap-x-10">
       <!-- Logo and brand -->
       <a href="{{ route('dashboard.index') }}" class="group flex items-center gap-x-2 transition-transform ease-in-out">
-        <div class="flex h-7 w-7 items-center justify-center rounded-sm bg-blue-600 p-1 transition-all duration-400 group-hover:-translate-y-0.5 group-hover:-rotate-3">
-          <x-lucide-users class="h-5 w-5 text-white" />
+        <div class="flex h-7 w-7 items-center justify-center transition-all duration-400 group-hover:-translate-y-0.5 group-hover:-rotate-3">
+          <img src="{{ asset('marketing/logo.png') }}" alt="PeopleOS logo" width="25" height="25" srcset="{{ asset('marketing/logo.png') }} 1x, {{ asset('marketing/logo@2x.png') }} 2x" />
         </div>
         <span class="text-lg font-semibold text-gray-900 dark:text-white">{{ config('app.name') }}</span>
       </a>
