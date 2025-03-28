1d514b5552348d984c1d0e519f5f3e31a7ba846c
Regis Freyd <djaiss@users.noreply.github.com>
2025-03-28 17:41:56 -0400

    chore: add inter

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}" />
+    <link rel="preconnect" href="https://rsms.me/" />
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
