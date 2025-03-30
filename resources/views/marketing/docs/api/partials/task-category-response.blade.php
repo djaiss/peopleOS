073600ac51a908b669693de1b2bfb251a819aae2
Regis Freyd <djaiss@users.noreply.github.com>
2025-03-29 20:09:53 -0400

    docs: query parameters for all api calls

---
+++ resources/views/marketing/docs/api/partials/task-category-response.blade.php
@@ -0,0 +1,29 @@
+<div class="pl-4">
+  "id":
+  <span class="text-rose-800">4</span>
+  ,
+</div>
+<div class="pl-4">
+  "object":
+  <span class="text-lime-700">"task_category"</span>
+  ,
+</div>
+<div class="pl-4">
+  "name":
+  <span class="text-lime-700">"Personal"</span>
+  ,
+</div>
+<div class="pl-4">
+  "color":
+  <span class="text-lime-700">"#FF0000"</span>
+  ,
+</div>
+<div class="pl-4">
+  "created_at":
+  <span class="text-rose-800">1715145600</span>
+  ,
+</div>
+<div class="pl-4">
+  "updated_at":
+  <span class="text-rose-800">1715145600</span>
+</div>
