2361275029ad7278cabbb02b05411603565049d3
Regis Freyd <djaiss@users.noreply.github.com>
2025-04-02 21:30:29 -0400

    Update MarketingControllerTest.php

--- tests/Feature/Controllers/Marketing/MarketingControllerTest.php
+++ tests/Feature/Controllers/Marketing/MarketingControllerTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Controllers\Marketing;
 
+use App\Services\FetchMergedPRs;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -15,11 +17,29 @@ class MarketingControllerTest extends TestCase
     #[Test]
     public function it_shows_the_homepage_of_marketing_site(): void
     {
+        $mockPRs = [
+            [
+                'number' => 123,
+                'title' => 'Test PR Title',
+                'merged_at' => '2024-03-20',
+                'body' => 'Test PR Description',
+                'url' => 'https://github.com/test/123'
+            ]
+        ];
+
+        Cache::partialMock()
+            ->shouldReceive('remember')
+            ->withArgs(function ($key, $ttl, $callback) {
+                return $key === 'github_pull_requests';
+            })
+            ->once()
+            ->andReturn($mockPRs);
+
         $response = $this->get('/')
             ->assertOk();
 
+        $response->assertViewHas('pullRequests', $mockPRs);
         $response->assertViewHas('accountNumbers');
-        $response->assertViewHas('pullRequests');
         $response->assertViewHas('marketingPage');
     }
 }
