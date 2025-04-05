2812422e230511e0b322f3e143ec9a688325d2d7
Regis Freyd <djaiss@users.noreply.github.com>
2025-04-05 02:12:37 +0000

    fix: fix inter blocking the render of the page (#195)

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -1,7 +1,18 @@
 <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}" />
 
-<link rel="preconnect" href="https://rsms.me/" />
-<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+<link rel="preconnect" href="https://rsms.me/" crossorigin>
+<link rel="preload" href="https://rsms.me/inter/inter.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="https://rsms.me/inter/inter.css"></noscript>
+
+<style>
+  /* Fallback font while Inter loads */
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+  }
+</style>
 
 <title>{{ config('app.name', 'Laravel') }}</title>
 <meta name="description" content="PeopleOS is a software to document your life and relationships." />
