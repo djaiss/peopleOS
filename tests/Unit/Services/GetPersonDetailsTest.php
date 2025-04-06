454f047b4cd6a0bd8b408d332d0a4968a1b77621
Regis Freyd <djaiss@users.noreply.github.com>
2025-04-06 19:31:49 -0400

    feat: add physical appearance details to person overview

--- tests/Unit/Services/GetPersonDetailsTest.php
+++ tests/Unit/Services/GetPersonDetailsTest.php
@@ -43,10 +43,41 @@ class GetPersonDetailsTest extends TestCase
             'person',
             'persons',
             'encounters',
+            'physicalAppearance',
         ]);
 
         $this->assertEquals(2, $array['encounters']['currentYearCount']);
         $this->assertEquals(2, $array['encounters']['previousYearCount']);
         $this->assertCount(4, $array['encounters']['latestSeen']);
+    }
+
+    #[Test]
+    public function it_returns_the_physical_appearance_details(): void
+    {
+        $user = User::factory()->create();
+
+        $person = Person::factory()->create([
+            'account_id' => $user->account_id,
+            'height' => '6\'0"',
+            'weight' => '180 lbs',
+            'build' => 'Athletic',
+            'skin_tone' => 'Fair',
+            'face_shape' => 'Oval',
+            'eye_color' => 'Blue',
+        ]);
+
+        $array = (new GetPersonDetails(
+            user: $user,
+            person: $person,
+        ))->getPhysicalAppearanceDetails();
+
+        $this->assertArrayHasKeys($array, [
+            'height',
+            'weight',
+            'build',
+            'skin_tone',
+            'face_shape',
+            'eye_color',
+        ]);
     }
 }
