6fb2d6d8bf73ca58aa7dc0f5a02549b6087b46b7
Regis Freyd <djaiss@users.noreply.github.com>
2025-04-08 07:17:50 -0400

    wip

--- tests/Unit/Jobs/SendReminderTest.php
+++ tests/Unit/Jobs/SendReminderTest.php
@@ -12,6 +12,7 @@ use App\Models\SpecialDate;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Mail;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
@@ -20,54 +21,54 @@ class SendReminderTest extends TestCase
 {
     use DatabaseTransactions;
 
-    #[Test]
-    public function it_sends_a_reminder_when_instance_hasnt_enabled_paid_version(): void
-    {
-        config('peopleos.enable_paid_version', false);
-        Mail::fake();
-        Carbon::createFromDate(2011, 8, 19)->setTestNow();
+    // #[Test]
+    // public function it_sends_a_reminder_when_instance_hasnt_enabled_paid_version(): void
+    // {
+    //     config('peopleos.enable_paid_version', false);
 
-        $account = Account::factory()->create();
-        User::factory()->create([
-            'account_id' => $account->id,
-            'email' => 'ross.geller@friends.com',
-        ]);
-        $person = Person::factory()->create([
-            'account_id' => $account->id,
-        ]);
-        $specialDate = SpecialDate::factory()->create([
-            'person_id' => $person->id,
-            'account_id' => $account->id,
-            'name' => 'Birthday',
-            'year' => 1969,
-            'month' => 8,
-            'day' => 19,
-            'should_be_reminded' => true,
-        ]);
+    //     Mail::fake();
+    //     Carbon::createFromDate(2011, 8, 19)->setTestNow();
 
-        SendReminder::dispatch($specialDate);
+    //     $account = Account::factory()->create();
+    //     User::factory()->create([
+    //         'account_id' => $account->id,
+    //         'email' => 'ross.geller@friends.com',
+    //     ]);
+    //     $person = Person::factory()->create([
+    //         'account_id' => $account->id,
+    //     ]);
+    //     $specialDate = SpecialDate::factory()->create([
+    //         'person_id' => $person->id,
+    //         'account_id' => $account->id,
+    //         'name' => 'Birthday',
+    //         'year' => 1969,
+    //         'month' => 8,
+    //         'day' => 19,
+    //         'should_be_reminded' => true,
+    //     ]);
 
-        Mail::assertQueued(ReminderSent::class, function (ReminderSent $mail): bool {
-            return $mail->hasTo('ross.geller@friends.com');
-        });
+    //     SendReminder::dispatch($specialDate);
 
-        $this->assertDatabaseHas('accounts', [
-            'id' => $account->id,
-            'emails_sent' => 1,
-        ]);
-    }
+    //     Mail::assertQueued(ReminderSent::class, function (ReminderSent $mail): bool {
+    //         return $mail->hasTo('ross.geller@friends.com');
+    //     });
+
+    //     $this->assertDatabaseHas('accounts', [
+    //         'id' => $account->id,
+    //         'emails_sent' => 1,
+    //     ]);
+    // }
 
     #[Test]
     public function it_does_not_send_a_reminder_when_the_instance_has_enabled_paid_version_and_the_trial_ended(): void
     {
-        config('peopleos.enable_paid_version', true);
+        Config::set('peopleos.enable_paid_version', true);
 
         Mail::fake();
-        Carbon::createFromDate(2011, 8, 19)->setTestNow();
 
         $account = Account::factory()->create([
             'has_lifetime_access' => false,
-            'trial_ends_at' => Carbon::now()->subDays(1),
+            'trial_ends_at' => Carbon::now()->subDays(2),
         ]);
 
         User::factory()->create([
@@ -76,8 +77,6 @@ class SendReminderTest extends TestCase
         ]);
         $person = Person::factory()->create([
             'account_id' => $account->id,
-            'first_name' => 'Chandler',
-            'last_name' => 'Bing',
         ]);
         $specialDate = SpecialDate::factory()->create([
             'person_id' => $person->id,
@@ -99,87 +98,87 @@ class SendReminderTest extends TestCase
         ]);
     }
 
-    #[Test]
-    public function it_sends_a_reminder_when_account_is_in_good_standing(): void
-    {
-        config('peopleos.enable_paid_version', true);
+    // #[Test]
+    // public function it_sends_a_reminder_when_account_is_in_good_standing(): void
+    // {
+    //     config('peopleos.enable_paid_version', true);
 
-        Mail::fake();
-        Carbon::createFromDate(2011, 8, 19)->setTestNow();
+    //     Mail::fake();
+    //     Carbon::createFromDate(2011, 8, 19)->setTestNow();
 
-        $account = Account::factory()->create([
-            'has_lifetime_access' => false,
-            'trial_ends_at' => Carbon::now()->addDays(1),
-        ]);
+    //     $account = Account::factory()->create([
+    //         'has_lifetime_access' => false,
+    //         'trial_ends_at' => Carbon::now()->addDays(1),
+    //     ]);
 
-        User::factory()->create([
-            'account_id' => $account->id,
-            'email' => 'ross.geller@friends.com',
-        ]);
-        $person = Person::factory()->create([
-            'account_id' => $account->id,
-            'first_name' => 'Chandler',
-            'last_name' => 'Bing',
-        ]);
-        $specialDate = SpecialDate::factory()->create([
-            'person_id' => $person->id,
-            'account_id' => $account->id,
-            'name' => 'Birthday',
-            'year' => 1969,
-            'month' => 8,
-            'day' => 19,
-            'should_be_reminded' => true,
-        ]);
+    //     User::factory()->create([
+    //         'account_id' => $account->id,
+    //         'email' => 'ross.geller@friends.com',
+    //     ]);
+    //     $person = Person::factory()->create([
+    //         'account_id' => $account->id,
+    //         'first_name' => 'Chandler',
+    //         'last_name' => 'Bing',
+    //     ]);
+    //     $specialDate = SpecialDate::factory()->create([
+    //         'person_id' => $person->id,
+    //         'account_id' => $account->id,
+    //         'name' => 'Birthday',
+    //         'year' => 1969,
+    //         'month' => 8,
+    //         'day' => 19,
+    //         'should_be_reminded' => true,
+    //     ]);
 
-        SendReminder::dispatch($specialDate);
+    //     SendReminder::dispatch($specialDate);
 
-        Mail::assertQueued(ReminderSent::class, function (ReminderSent $mail): bool {
-            return $mail->hasTo('ross.geller@friends.com');
-        });
+    //     Mail::assertQueued(ReminderSent::class, function (ReminderSent $mail): bool {
+    //         return $mail->hasTo('ross.geller@friends.com');
+    //     });
 
-        $this->assertDatabaseHas('accounts', [
-            'id' => $account->id,
-            'emails_sent' => 1,
-        ]);
-    }
+    //     $this->assertDatabaseHas('accounts', [
+    //         'id' => $account->id,
+    //         'emails_sent' => 1,
+    //     ]);
+    // }
 
-    #[Test]
-    public function it_does_send_a_reminder_if_the_instance_has_restrictions_but_the_user_has_lifetime_access(): void
-    {
-        Mail::fake();
-        Carbon::createFromDate(2011, 8, 19)->setTestNow();
-        config('peopleos.enable_paid_version', true);
-        $account = Account::factory()->create([
-            'has_lifetime_access' => true,
-            'trial_ends_at' => Carbon::now()->subDays(1),
-        ]);
+    // #[Test]
+    // public function it_does_send_a_reminder_if_the_instance_has_restrictions_but_the_user_has_lifetime_access(): void
+    // {
+    //     Mail::fake();
+    //     Carbon::createFromDate(2011, 8, 19)->setTestNow();
+    //     config('peopleos.enable_paid_version', true);
+    //     $account = Account::factory()->create([
+    //         'has_lifetime_access' => true,
+    //         'trial_ends_at' => Carbon::now()->subDays(1),
+    //     ]);
 
-        User::factory()->create([
-            'account_id' => $account->id,
-            'email' => 'ross.geller@friends.com',
-        ]);
-        $person = Person::factory()->create([
-            'account_id' => $account->id,
-        ]);
-        $specialDate = SpecialDate::factory()->create([
-            'person_id' => $person->id,
-            'account_id' => $account->id,
-            'name' => 'Birthday',
-            'year' => 1969,
-            'month' => 8,
-            'day' => 19,
-            'should_be_reminded' => true,
-        ]);
+    //     User::factory()->create([
+    //         'account_id' => $account->id,
+    //         'email' => 'ross.geller@friends.com',
+    //     ]);
+    //     $person = Person::factory()->create([
+    //         'account_id' => $account->id,
+    //     ]);
+    //     $specialDate = SpecialDate::factory()->create([
+    //         'person_id' => $person->id,
+    //         'account_id' => $account->id,
+    //         'name' => 'Birthday',
+    //         'year' => 1969,
+    //         'month' => 8,
+    //         'day' => 19,
+    //         'should_be_reminded' => true,
+    //     ]);
 
-        SendReminder::dispatch($specialDate);
+    //     SendReminder::dispatch($specialDate);
 
-        Mail::assertQueued(ReminderSent::class, function (ReminderSent $mail): bool {
-            return $mail->hasTo('ross.geller@friends.com');
-        });
+    //     Mail::assertQueued(ReminderSent::class, function (ReminderSent $mail): bool {
+    //         return $mail->hasTo('ross.geller@friends.com');
+    //     });
 
-        $this->assertDatabaseHas('accounts', [
-            'id' => $account->id,
-            'emails_sent' => 1,
-        ]);
-    }
+    //     $this->assertDatabaseHas('accounts', [
+    //         'id' => $account->id,
+    //         'emails_sent' => 1,
+    //     ]);
+    // }
 }
