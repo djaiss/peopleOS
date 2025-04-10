b7c61aea5a3baa8ca1cca3947c94ab11feed89a4
Regis Freyd <djaiss@users.noreply.github.com>
2025-04-10 07:31:10 -0400

    Create 2025_04_10_020816_create_life_events_table.php

---
+++ database/migrations/2025_04_10_020816_create_life_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('life_events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->unsignedBigInteger('person_id')->nullable();
+            $table->unsignedBigInteger('special_date_id')->nullable();
+            $table->text('description');
+            $table->text('comment')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('bg_color')->nullable();
+            $table->string('text_color')->nullable();
+            $table->datetime('happened_at')->nullable();
+            $table->timestamps();
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('person_id')->references('id')->on('persons')->onDelete('set null');
+            $table->foreign('special_date_id')->references('id')->on('special_dates')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('life_events');
+    }
+};
