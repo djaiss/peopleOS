dd085375bb16bda1559812c532cdf5c15b01db44
Regis Freyd <djaiss@users.noreply.github.com>
2025-04-26 12:34:06 -0400

    feat: services to manage testimonies in backend (#254)

---
+++ app/Services/RejectMarketingTestimony.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Enums\MarketingTestimonyStatus;
+use App\Models\MarketingTestimony;
+use App\Models\User;
+use Exception;
+
+class RejectMarketingTestimony
+{
+    public function __construct(
+        private readonly User $user,
+        private readonly MarketingTestimony $testimony,
+    ) {}
+
+    public function execute(): MarketingTestimony
+    {
+        $this->validate();
+        $this->updateStatus();
+
+        return $this->testimony;
+    }
+
+    private function validate(): void
+    {
+        if (! $this->user->is_instance_admin) {
+            throw new Exception('User must be an instance administrator to reject a testimony.');
+        }
+    }
+
+    private function updateStatus(): void
+    {
+        $this->testimony->update([
+            'status' => MarketingTestimonyStatus::REJECTED->value,
+        ]);
+    }
+}
