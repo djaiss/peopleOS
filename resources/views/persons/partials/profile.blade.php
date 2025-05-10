fccddc6f3e48f2a25eb1d9638086844da2911125
Regis Freyd <djaiss@users.noreply.github.com>
2025-05-10 08:47:19 -0400

    feat: create love relationships (#273)

--- resources/views/persons/partials/profile.blade.php
+++ resources/views/persons/partials/profile.blade.php
@@ -5,7 +5,7 @@
 ?>
 
 <div class="flex h-[calc(100vh-48px)] flex-col overflow-hidden bg-white">
-  <!-- Contact Header -->
+  <!-- Contact header -->
   <div class="border-b border-gray-200 p-6">
     <!-- name + title + age -->
     <div id="profile-header" class="mb-6 flex items-center gap-4">
@@ -13,8 +13,10 @@
         <img class="h-16 w-16 rounded-full object-cover p-[0.1875rem] shadow-sm ring-1 ring-slate-900/10" src="{{ $person->getAvatar(64) }}" srcset="{{ $person->getAvatar(64) }}, {{ $person->getAvatar(128) }} 2x" alt="{{ $person->name }}" loading="lazy" />
       </div>
       <div class="flex min-w-0 flex-col gap-1">
+        <!-- name -->
         <h1 class="truncate text-xl font-semibold">{{ $person->name }}</h1>
 
+        <!-- job -->
         @php
           $job = $person->job();
         @endphp
@@ -23,15 +25,16 @@
           <span class="text-sm text-gray-600">{{ $job }}</span>
         @endif
 
+        <!-- age -->
         @if ($person->age)
           <span class="text-sm text-gray-600">{{ $person->age }}</span>
         @endif
       </div>
     </div>
 
-    <!-- Personal Details -->
+    <!-- Personal details -->
     <div class="space-y-2">
-      <!-- Relationship Status -->
+      <!-- Relationship status -->
       <div class="flex items-center gap-2">
         <x-lucide-heart class="h-4 w-4 text-rose-500" />
         @include('persons.partials.marital-statuses', ['person' => $person])
@@ -51,7 +54,7 @@
     </div>
   </div>
 
-  <!-- Navigation Menu -->
+  <!-- Navigation menu -->
   <nav class="border-b border-gray-200">
     <div class="flex flex-col">
       <a href="{{ route('person.show', ['slug' => $person['slug']]) }}" class="{{ request()->routeIs('person.show') ? 'border-blue-500 bg-blue-50' : '' }} group flex items-center gap-3 border-l-2 border-transparent px-4 py-3 hover:bg-gray-50">
@@ -66,12 +69,12 @@
         <x-lucide-book-open class="{{ request()->routeIs('person.note.index') ? 'text-blue-500' : 'text-gray-500' }} h-4 w-4 transition-all duration-400 grouphover:-translate-y-0.5 group-hover:-rotate-3" />
         <span class="{{ request()->routeIs('person.note.index') ? 'text-blue-700' : 'text-gray-600' }} text-sm font-medium">Notes</span>
       </a>
-      {{--
-        <a href="{{ route('person.family.index', ['slug' => $person['slug']]) }}" class="{{ request()->routeIs('person.family.index') ? 'border-blue-500 bg-blue-50' : '' }} group flex items-center gap-3 border-l-2 border-transparent px-4 py-3 hover:bg-gray-50">
+
+      <a href="{{ route('person.family.index', ['slug' => $person['slug']]) }}" class="{{ request()->routeIs('person.family.index') ? 'border-blue-500 bg-blue-50' : '' }} group flex items-center gap-3 border-l-2 border-transparent px-4 py-3 hover:bg-gray-50">
         <x-lucide-person-standing class="{{ request()->routeIs('person.family.index') ? 'text-blue-500' : 'text-gray-500' }} h-4 w-4 transition-all duration-400 grouphover:-translate-y-0.5 group-hover:-rotate-3" />
-        <span class="{{ request()->routeIs('persons.family.index') ? 'text-blue-700' : 'text-gray-600' }} text-sm font-medium">Family</span>
-        </a>
-      --}}
+        <span class="{{ request()->routeIs('person.family.index') ? 'text-blue-700' : 'text-gray-600' }} text-sm font-medium">{{ __('Love, family & friends') }}</span>
+      </a>
+
       <a href="{{ route('person.reminder.index', ['slug' => $person['slug']]) }}" class="{{ request()->routeIs('person.reminder.index') ? 'border-blue-500 bg-blue-50' : '' }} group flex items-center gap-3 border-l-2 border-transparent px-4 py-3 hover:bg-gray-50">
         <x-lucide-bell class="{{ request()->routeIs('person.reminder.index') ? 'text-blue-500' : 'text-gray-500' }} h-4 w-4 transition-all duration-400 grouphover:-translate-y-0.5 group-hover:-rotate-3" />
         <span class="{{ request()->routeIs('person.reminder.index') ? 'text-blue-700' : 'text-gray-600' }} text-sm font-medium">{{ __('Tasks and reminders') }}</span>
