2f3b55f484389d40b4118528a3b3e3d212d89da0
Regis Freyd <djaiss@users.noreply.github.com>
2025-06-03 08:35:08 -0400

    feat: send email when trial is ending in 5 days (#298)

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Jobs\CountDailyUsers;
 use App\Jobs\DeleteInactiveAccounts;
 use App\Jobs\ProcessReminders;
+use App\Jobs\ProcessUsersTrialEndingSoon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -16,3 +17,4 @@ Artisan::command('inspire', function (): void {
 Schedule::job(new CountDailyUsers(), 'low')->dailyAt('00:20');
 Schedule::job(new ProcessReminders(), 'low')->dailyAt('00:00');
 Schedule::job(new DeleteInactiveAccounts(), 'low')->dailyAt('00:30');
+Schedule::job(new ProcessUsersTrialEndingSoon(), 'low')->dailyAt('01:00');
