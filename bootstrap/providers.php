aca0229ed2059860d097ba46df0750b451c67c88
Regis Freyd <djaiss@users.noreply.github.com>
2025-07-24 08:09:29 -0400

    feat: prevent disposable emails from registering

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use LaravelDisposableEmail\LaravelDisposableEmailServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
+    LaravelDisposableEmailServiceProvider::class,
 ];
